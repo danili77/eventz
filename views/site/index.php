12dd1114c51b32d6561cdbee22681acc30a57f03
Daniel Lorenzo <daniel.lorenzo@iesdonana.org>
2017-11-17 19:51:34 +0100

    Añadido microdatos a pagina de inicio

--- views/site/index.php
+++ views/site/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\assets\IndexAsset;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
@@ -16,24 +17,33 @@ $this->title = 'Eventz';
     <div id="contenido">
         <section>
           <div>
-              <article>
-                <h2>Eventz</h2>
-                  <p>Eventz es un proyecto desarrollado por Daniel Lorenzo Ibáñez,con el objetivo de ofrecer una herramienta que facilite la gestión de eventos a los usuarios.La idea de Eventz comienza a desarrollarse en Sanlúcar de Barrameda(Cádiz),como proyecto integrado del Ciclo Formativo de Grado Superior de Desarrollo de Aplicaciones Web.</p>
-                  <p>Dispone de las siguientes caracteristicas: </p>
-                  <ul>
-                      <li>Gestión de Eventos.</li>
-                      <li>Paso de listado de eventos a documento PDF.</li>
-                      <li>Gestión de Comentarios sobre eventos.</li>
-                      <li>Calendario interactivo.Donde es posible ver los eventos y crear nuevos.</li>
-                      <li>Gestión de Usuarios(Solo el administrador de la página)</li>
-                  </ul>
-                </article>
-                  <article>
-                    <h2>Promo</h2>
-                      <video controls>
-                          <source src="/videos/turismo.mp4" type="video/mp4">
-                      </video>
+              <div class="site-index" itemscope itemtype="https://schema.org/WebApplication">
+                <article>
+                  <h2>Eventz</h2>
+                    <p><span itemprop="description"><span itemprop="name">Eventz</span> es una <span itemprop="applicationCategory">aplicación web</span> desarrollada por<span itemprop="creator">Daniel Lorenzo Ibáñez</span>,con el objetivo de ofrecer una herramienta que facilite la gestión de eventos a los usuarios.La idea de Eventz comienza a desarrollarse en Sanlúcar de Barrameda(Cádiz) en el año <span itemprop="dateCreated">2017</span>,como proyecto integrado final del Ciclo Formativo de Grado Superior de Desarrollo de Aplicaciones Web.Se puede abrir en cualquier navegador de <span itemprop="operatingSystem">cualquier sistema operativo.</span></p>
+                    <p>Dispone de las siguientes caracteristicas: </p>
+                    <ul>
+                        <li>Gestión de Eventos.</li>
+                        <li>Paso de listado de eventos a documento PDF.</li>
+                        <li>Gestión de Comentarios sobre eventos.</li>
+                        <li>Calendario interactivo.Donde es posible ver los eventos y crear nuevos.</li>
+                        <li>Gestión de Usuarios(Solo el administrador de la página)</li>
+                    </ul>
+
                   </article>
+                    <article>
+                      <h2>Promo</h2>
+                        <video controls>
+                            <source src="/videos/turismo.mp4" type="video/mp4">
+                        </video><br><br>
+                        <p>Si dispone de alguna consulta o sugerencia, no dude en
+                        ponerse en contacto con nosotros a través de la sección de
+                        <span itemprop="url"><?= Html::a('Contacto', ['contact']) ?></span>
+
+
+                        </p>
+                    </article>
+                </div>
             </div>
         </section>
       </div>

f3f21f97dce23a37fa060d71b95b55a54cba0869
Daniel Lorenzo <daniel.lorenzo@iesdonana.org>
2017-12-18 01:59:36 +0100

    heroku bd

---
+++ db/e.sql
@@ -0,0 +1,6 @@
+
+insert into usuarios(nombre,email,password)
+values('dani','daniel.lorenzo@iesdonana.org','dani'),
+('pepe','pepe@gmail.com','pepe'),
+('maria','maria@gmail.com','maria'),
+('admin','admin@gmail.com','admin');
